790af7e447a311d5923cd5c274e89d7f58dcbea1
Billy Bunn <williammbunn@gmail.com>
2019-05-15 11:52:04 -0700

    finishes crappy free code camp tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tutorial';
+  todoArray = [];
+
+  addTodo(value) {
+    // console.log('addTodo');
+    this.todoArray.push(value);
+    console.log(this.todoArray);
+  }
+
+  deleteItem(todo) {
+    // console.log('deleteItem');
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
 }
